2ee5f8c02a9673e51d0275020a9333387d1075e7
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-06-20 15:51:41 +0800

    update search for null

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,25 +11,53 @@ function Products() {
     { name: 'Amino acids + Sorbitol', description: 'Solution for Infusion (IV)', src:'https://www.siamclinicphuket.com/wp-content/uploads/2023/07/kidmin2-724x1024.png', category: 'pharmaceutical', type: 'Solution' },
     { name: 'Amikacin (as sulfate)', description: '250mg/mL (500mg/mL) SFI (IM/IV)', src:'https://th.bing.com/th/id/R.cb116fe01417a9a49614195d5b1f30cc?rik=1nGM1cI2u1PF9g&riu=http%3a%2f%2favenciaworld.com%2fwp-content%2fuploads%2f2017%2f05%2f2016_Product_V_Amikacin-500-mg-per-2-mL-_front-ADJ-3.png&ehk=5EWJTAv71vrYMXC7PG3u7cRhaG6bvXPGIwHoO3njLRQ%3d&risl=&pid=ImgRaw&r=0', category: 'pharmaceutical', type: 'Solution' },
     { name: 'Amlodipine besilate', description: '5mg Tablet', src:'https://www.sidomuncul.co.id/assets/images/product/Berlico/Prescription-Ethical/Amlodipine.png', category: 'pharmaceutical', type: 'Tablet' },
+    
     { name: 'Amlodipine besilate', description: '10mg Tablet', src:'https://imfarmindfarmasi.com/wp-content/uploads/2020/02/amlodipine_kaplet_10_mg_allufoil_strip-1024x768.png', category: 'pharmaceutical', type: 'Tablet' },
     { name: 'Amoxicillin trihydrate', description: '500mg Capsule', src:'', category: 'pharmaceutical', type: 'Capsule' },
     { name: 'Ampicillin sodium', description: '1g PFI (IM/IV)', src:'', category: 'pharmaceutical', type: 'Injection' },
     { name: 'Ampicillin sodium', description: '500mg PFI (IM/IV)', src:'', category: 'pharmaceutical', type: 'Injection' },
-    { name: 'Ampicillin sodium + Sulbactam sodium', description: '750mg PFI (IM/IV)', src:'', category: 'pharmaceutical', type: 'Injection' },
-    { name: 'Ascorbic Acid', description: '500mg Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
-    { name: 'Ascorbic Acid', description: '1g Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
-    { name: 'Ascorbic Acid', description: '500mg Capsule', src:'', category: 'pharmaceutical', type: 'Capsule' },
-    { name: 'Bisacodyl', description: '5mg Enteric Coated Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
-    { name: 'Bupivacaine HCl in 8% Dextrose', description: '5mg/mL Solution', src:'', category: 'pharmaceutical', type: 'Solution' },
-    { name: 'Calcium + Cholecalciferol', description: '50mg/200IU Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
-    { name: 'Carbocisteine', description: '500mg Capsule', src:'', category: 'pharmaceutical', type: 'Capsule' },
-    { name: 'Cefazolin sodium', description: '1g PFI (IM/IV)', src:'', category: 'pharmaceutical', type: 'Injection' },
-    { name: 'Cefdinir', description: '300mg Capsule', src:'', category: 'pharmaceutical', type: 'Capsule' },
-    { name: 'Cefepime hydrochloride', description: '1g PFI (IM/IV)', src:'', category: 'pharmaceutical', type: 'Injection' },
-    { name: 'Cefixime trihydrate', description: '200mg FC Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
-    { name: 'Zinc', description: '50mg Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
-    { name: 'Water for Injection', description: 'Solution for Injection', src:'', category: 'pharmaceutical', type: 'Solution' }
+
+    { name: '3 SN Phosphatidyl Choline + B Vitamins', description: 'Dietary Supplement Softgel Capsule', src: '', category: 'supplements', type: 'Softgel Capsule' },
+    { name: 'Alpha-Lipoic Acid, Gamma Linoleic Acid, Lutein and Zeaxanthin with B-Vitamins', description: 'Food Supplement Softgel Capsule', src: '', category: 'supplements', type: 'Softgel Capsule' },
+    { name: 'Alpha Lipoic Acid (ALA)', description: '600mg Food Supplement Capsule', src: '', category: 'supplements', type: 'Capsule' },
+    { name: 'Amino Acids', description: '600mg Dietary Supplement Softgel Capsule', src: '', category: 'supplements', type: 'Softgel Capsule' },
+    
+    { name: 'Amino Acids with B-Vitamins', description: '800mg Food Supplement Softgel Capsule', src: '', category: 'supplements', type: 'Softgel Capsule' },
+    { name: 'Amino Acids with B-Vitamins', description: 'Dietary Supplement Softgel Capsule', src: '', category: 'supplements', type: 'Softgel Capsule' },
+    { name: 'Ampalaya', description: '600mg Food Supplement Capsule', src: '', category: 'supplements', type: 'Capsule' },
+    { name: 'Ashitaba (Angelica keiske Koidzumi)', description: 'Dietary Supplement Capsule 500mg', src: '', category: 'supplements', type: 'Capsule' },
+    
+    { name: 'Air Compressing Nebulizer', description: 'Machine Only', src: '', category: 'devices', type: 'digital-equipments' },
+    { name: 'Aneroid Sphygmomanometer', description: '', src: '', category: 'devices', type: 'digital-equipments' },
+    { name: 'Digital Blood Pressure Monitor', description: '', src: '', category: 'devices', type: 'digital-equipments' },
+    { name: 'Digital Blood Pressure Monitor (Arm Style)', description: '', src: '', category: 'devices', type: 'digital-equipments' },
+  
+    { name: 'Absorbent Cotton Balls', description: '0.2 to 5g', src: '', category: 'devices', type: 'disposable-supplies' },
+    { name: 'Absorbent Cotton Balls', description: '', src: '', category: 'devices', type: 'disposable-supplies' },
+    { name: 'Adult Diaper Regular', description: '', src: '', category: 'devices', type: 'disposable-supplies' },
+    { name: 'Adult Diaper Premium Pull-Ups', description: '', src: '', category: 'devices', type: 'disposable-supplies' },
+
+    { name: 'Arm Crutch', description: '', src: '', category: 'devices', type: 'mobility-aid' },
+    { name: 'Cane', description: '', src: '', category: 'devices', type: 'mobility-aid' },
+    { name: 'Cane with Chair', description: '', src: '', category: 'devices', type: 'mobility-aid' },
+    { name: 'Commode Chair', description: '', src: '', category: 'devices', type: 'mobility-aid' },
   ]
+
+  const CategoryFilter = products.filter(product => 
+    product.category.toLowerCase().includes(ID.toLowerCase())
+  );
+
+  let filteredProducts = CategoryFilter.filter(product => {
+    if (search != '') {
+      return (
+        product.name.toLowerCase().includes(search.toLowerCase()) ||
+        product.description.toLowerCase().includes(search.toLowerCase()) ||
+        product.type.toLowerCase().includes(search.toLowerCase())
+      );
+    } else {
+      return true;
+    }
+  });
 
   useEffect(() => {
     if (!value) return;
@@ -39,16 +67,6 @@ function Products() {
 
     return () => clearTimeout(handler);
   }, [value]);
-
-  const filteredProducts = search
-  ? products.filter(
-      product =>
-      product.name.toLowerCase().includes(search.toLowerCase()) ||
-      product.description.toLowerCase().includes(search.toLowerCase()) ||
-      product.category.toLowerCase().includes(search.toLowerCase()) ||
-      product.type.toLowerCase().includes(search.toLowerCase())
-    )
-  : products;
 
   return (
     <div className='bg-gray-100 dark:bg-black py-5!'
@@ -64,7 +82,7 @@ function Products() {
       <div className="md:max-w-[80dvw] lg:max-w-[75dvw] mx-auto! flex flex-col gap-5 items-center min-h-[88dvh]">
         <div 
           style={{
-            padding: '20px 40px 15px 30px',
+            padding: '15px 60px 15px 50px',
             backgroundColor: 'rgba(255, 255, 255, 0.1)',
             boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
             border: '1px solid rgba(255, 255, 255, 0.5)',
@@ -81,6 +99,7 @@ function Products() {
             className='border border-t-0 border-x-0 outline-0 w-full text-[#7A7A7A]'
             value={value}
             onChange={e => setValue(e.target.value)}
+            onInput={e => e.target.value == '' ? setSearch('') : null}
           />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
