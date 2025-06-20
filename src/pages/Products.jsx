6dccc64fb11595339eeeaa9bc858102c412d5aa5
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-06-20 13:49:53 +0800

    Implement search functionality in Products component: add useEffect for debouncing input, filter products based on search criteria, and enhance layout for better user experience

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,10 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Products() {
   const { ID } = useParams();
+  const [value, setValue] = useState('');
+  const [search, setSearch] = useState('');
 
   const products = [
     { name: 'Acarbose', description: '500mg Tablet', src:'https://5.imimg.com/data5/SELLER/Default/2022/11/GK/FX/VI/160457072/acarbose-50-mg-1000x1000.png', category: 'pharmaceutical', type: 'Tablet' },
@@ -27,7 +30,26 @@ function Products() {
     { name: 'Zinc', description: '50mg Tablet', src:'', category: 'pharmaceutical', type: 'Tablet' },
     { name: 'Water for Injection', description: 'Solution for Injection', src:'', category: 'pharmaceutical', type: 'Solution' }
   ]
-  
+
+  useEffect(() => {
+    if (!value) return;
+    const handler = setTimeout(() => {
+      setSearch(value);
+    }, 300);
+
+    return () => clearTimeout(handler);
+  }, [value]);
+
+  const filteredProducts = search
+  ? products.filter(
+      product =>
+      product.name.toLowerCase().includes(search.toLowerCase()) ||
+      product.description.toLowerCase().includes(search.toLowerCase()) ||
+      product.category.toLowerCase().includes(search.toLowerCase()) ||
+      product.type.toLowerCase().includes(search.toLowerCase())
+    )
+  : products;
+
   return (
     <div className='bg-gray-100 dark:bg-black py-5!'
       style={{
@@ -42,7 +64,7 @@ function Products() {
       <div className="md:max-w-[80dvw] lg:max-w-[75dvw] mx-auto! flex flex-col gap-5 items-center min-h-[88dvh]">
         <div 
           style={{
-            padding: '20px 40px 15px 40px',
+            padding: '20px 40px 15px 30px',
             backgroundColor: 'rgba(255, 255, 255, 0.1)',
             boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
             border: '1px solid rgba(255, 255, 255, 0.5)',
@@ -56,11 +78,67 @@ function Products() {
           <input 
             type='search' 
             style={{ fontFamily: "Karla" }}
-            className='border border-t-0 border-x-0 outline-0 w-full h-[15px] text-[#7A7A7A]'
+            className='border border-t-0 border-x-0 outline-0 w-full text-[#7A7A7A]'
+            value={value}
+            onChange={e => setValue(e.target.value)}
           />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {products.map((product, index) => (
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product, index) => (
+              <div 
+                style={{
+                  padding: '50px 35px',
+                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                  boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
+                  border: '1px solid rgba(255, 255, 255, 0.5)',
+                  borderRadius: '5px',
+                  borderRight: '1px solid rgba(255, 255, 255, 0.2)',
+                  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+                  backdropFilter: 'blur(5px)'
+                }}
+                className='border border-gray-100 rounded-lg bg-[rgba(255,255,255,0.5)] flex flex-col gap-3 p-5!' key={index}
+              >
+                {
+                  product.src ? (
+                    <>
+                      <img
+                        src={product.src}
+                        alt='Image' 
+                        className='object-contain h-[200px] w-[200px] mx-auto! bg-white'
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <div className='relative overflow-hidden flex items-center justify-center h-[200px] w-[200px] mx-auto! bg-white'>
+                        <div className='absolute content-[""] w-[60px] h-[60px] rounded-full bg-red-500 flex items-center justify-center'>
+                          <div className='absolute content-[""] w-[40px] h-[40px] rounded-full bg-white flex items-center justify-center'>
+                            <div className='content-[""] -rotate-45 h-[10px] w-full bg-red-500'></div>
+                          </div>
+                        </div>
+                        <span 
+                          className="absolute w-[500px] -right-[93%] bottom-[60%] py-2! text-black text-center bg-[#6EC1E4] rotate-45"
+                          style={{ fontFamily: "Changa" }}
+                        >NO IMAGE AVAILABLE</span>
+                      </div>
+                    </>
+                  )
+                }
+                <span 
+                  className="mt-3! text-center font-bold text-[#7A7A7A] dark:text-gray-600"
+                  style={{ fontFamily: "Karla" }}
+                >
+                    {product.name}
+                </span>
+                <span 
+                  className="text-left text-[#7A7A7A] dark:text-gray-300"
+                  style={{ fontFamily: "Karla" }}
+                >
+                    {product.description}
+                </span>
+              </div>
+            ))
+          ) : (
             <div 
               style={{
                 padding: '50px 35px',
@@ -72,42 +150,33 @@ function Products() {
                 borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
                 backdropFilter: 'blur(5px)'
               }}
-              className='border border-gray-100 rounded-lg bg-[rgba(255,255,255,0.5)] flex flex-col gap-3 p-5!' key={index}>
-              {
-                product.src ? (
-                  <>
-                    <img
-                      src={product.src}
-                      alt='Image' 
-                      className='object-contain h-[200px] w-[200px] mx-auto! bg-white'
-                    />
-                  </>
-                ) : (
-                  <>
-                    <div className='relative overflow-hidden flex items-center justify-center h-[200px] w-[200px] mx-auto! bg-white'>
-                      <span 
-                        className="absolute w-[500px] -right-[93%] bottom-[60%] py-2! text-black text-center bg-[#6EC1E4] rotate-45"
-                        style={{ fontFamily: "Changa" }}
-                      >NO IMAGE AVAILABLE</span>
-                    </div>
-                  </>
-                )
-              }
-              
+              className='border border-gray-100 rounded-lg bg-[rgba(255,255,255,0.5)] flex flex-col gap-3 p-5!'
+            >
+              <div className='relative overflow-hidden flex items-center justify-center h-[200px] w-[200px] mx-auto! bg-white'>
+                <div className='absolute content-[""] w-[60px] h-[60px] rounded-full bg-red-500 flex items-center justify-center'>
+                  <div className='absolute content-[""] w-[40px] h-[40px] rounded-full bg-white flex items-center justify-center'>
+                    <div className='content-[""] -rotate-45 h-[10px] w-full bg-red-500'></div>
+                  </div>
+                </div>
+                <span 
+                  className="absolute w-[500px] -right-[93%] bottom-[60%] py-2! text-black text-center bg-[#6EC1E4] rotate-45"
+                  style={{ fontFamily: "Changa" }}
+                >NOT AVAILABLE</span>
+              </div>
               <span 
-                className="mt-3! text-center font-bold text-[#7A7A7A] dark:text-gray-600"
-                style={{ fontFamily: "Karla" }}
-              >
-                  {product.name}
-              </span>
-              <span 
-                className="text-left text-[#7A7A7A] dark:text-gray-300"
-                style={{ fontFamily: "Karla" }}
-              >
-                  {product.description}
-              </span>
+                  className="mt-3! text-center font-bold text-[#7A7A7A] dark:text-gray-600"
+                  style={{ fontFamily: "Karla" }}
+                >
+                    NO PRODUCT AVAILABLE
+                </span>
+                <span 
+                  className="text-left text-[#7A7A7A] dark:text-gray-300"
+                  style={{ fontFamily: "Karla" }}
+                >
+                    Try searching something else.
+                </span>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
